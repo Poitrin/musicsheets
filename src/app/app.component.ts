8d8e18a50319c7f125f24d6e1bbab1274e97221c
Poitrin <dev@poitrin.com>
2017-05-13 19:32:00 +0200

    Update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor(translate: TranslateService) {
